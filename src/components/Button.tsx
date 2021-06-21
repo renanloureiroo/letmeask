b98b2525eefafb3938241a8e9ec263de1cc7c36d
Renan Loureiro <renanpl47@gmail.com>
2021-06-21 12:32:27 -0300

    project created and connected with firebase

---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+export const Button = () => {
+  const [counter, setCounter] = useState(0);
+
+  return <button onClick={() => setCounter(counter + 1)}>{counter}</button>;
+};
